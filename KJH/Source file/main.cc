7509a61ed8f7bf3688f24a76c0ec2a206e145320
sg05060 <99june18@naver.com>
2022-09-11 16:32:00 +0900

    week 1

--- KJH/Source file/main.cc
+++ KJH/Source file/main.cc
@@ -10,9 +10,9 @@ int main() {
     uint8_t***ifmap = init_3D_tensor(C,H,W);
     uint8_t**** filter = init_4D_tensor(Cout,C,K,K);
     FILE *fp_ifmap, *fp_filter, *fp_ofmap;
-	fp_ifmap = fopen("/home/sg05060/C/Week_1/data/ifmap.txt","w");
-	fp_filter = fopen("/home/sg05060/C/Week_1/data/filter.txt","w");
-	fp_ofmap = fopen("/home/sg05060/C/Week_1/data/ofmap.txt","w");
+	fp_ifmap = fopen("ifmap.txt","w");
+	fp_filter = fopen("filter.txt","w");
+	fp_ofmap = fopen("ofmap.txt","w");
 
     set_3D_tensor(ifmap,C,H,W,fp_ifmap);
     set_4D_tensor(filter,Cout,C,K,K,fp_filter);
