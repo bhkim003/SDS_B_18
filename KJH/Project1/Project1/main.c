baf7ea024c19db564d1167d3724c344ce8677c5d
sg05060 <99june18@naver.com>
2022-09-12 17:24:29 +0900

    add time

--- KJH/Project1/Project1/main.c
+++ KJH/Project1/Project1/main.c
@@ -9,6 +9,9 @@
 #include "function.h"
 
 int main() {
+
+    clock_t start, end;
+
     uint8_t*** ifmap = init_3D_tensor(C, H, W);
     uint8_t**** filter = init_4D_tensor(Cout, C, K, K);
     FILE* fp_ifmap, * fp_filter, * fp_ofmap;
@@ -19,7 +22,12 @@ int main() {
     set_3D_tensor(ifmap, C, H, W, fp_ifmap);
     set_4D_tensor(filter, Cout, C, K, K, fp_filter);
 
+    start = clock();
+
     uint8_t*** ofmap = convolution_2D(ifmap, filter, C, H, W, Cout, K, STRIDE, P, fp_ofmap);
+
+    end = clock();
+    printf("[time : %f]\n", (float)(end - start) / CLOCKS_PER_SEC);
 
     fclose(fp_ifmap);
     fclose(fp_filter);
